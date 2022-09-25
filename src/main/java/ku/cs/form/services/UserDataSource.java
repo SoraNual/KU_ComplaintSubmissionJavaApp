fc66aae3a0b21f1b3176d30a658d2fb2e34bcafe
phubestp <phubest.p@ku.th>
2022-09-25 21:52:22 +0700

    add upload pic sys in staff-register

--- src/main/java/ku/cs/form/services/UserDataSource.java
+++ src/main/java/ku/cs/form/services/UserDataSource.java
@@ -4,11 +4,11 @@ import ku.cs.form.models.*;
 
 import java.io.*;
 
-public class UserData {
+public class UserDataSource implements DataSource<UserList>{
     private String directoryName;
     private String fileName;
 
-    public UserData(String directoryName, String fileName) {
+    public UserDataSource(String directoryName, String fileName) {
         this.directoryName = directoryName;
         this.fileName = fileName;
         checkFileIsExisted();
@@ -73,5 +73,66 @@ public class UserData {
             }
         }
         return null;
+    }
+
+    @Override
+    public UserList readData() {
+        UserList userList = new UserList();
+        String filePath = directoryName + File.separator + fileName;
+        File file = new File(filePath);
+        FileReader reader = null;
+        BufferedReader buffer = null;
+
+        try {
+            reader = new FileReader(file);
+            buffer = new BufferedReader(reader);
+            String line = "";
+            while ((line = buffer.readLine()) != null) {
+
+                String[] data = line.split(",");
+                String class_name = data[2].trim(); // check Class
+
+                if(class_name.equals("staff")){
+
+                    Staff staff = new Staff(data[3].trim(), data[0].trim(), data[1].trim(),data[6].trim()); // add agency
+                    staff.setLoginAttempt(Integer.parseInt(data[5].trim()));
+                    staff.setUserStatus(data[4].trim());
+                    userList.addUser(staff);
+                }
+
+                else if(class_name.equals("admin")){
+                    Admin admin = new Admin(data[3].trim(), data[0].trim(), data[1].trim());
+                    admin.setLoginAttempt(Integer.parseInt(data[5].trim()));
+                    admin.setUserStatus(data[4].trim());
+                    userList.addUser(admin);
+                }
+
+                else if(class_name.equals("nisit")){
+                    Nisit nisit = new Nisit(data[3].trim(), data[0].trim(), data[1].trim());
+                    nisit.setLoginAttempt(Integer.parseInt(data[5].trim()));
+                    nisit.setUserStatus(data[4].trim());
+                    userList.addUser(nisit);
+                }
+            }
+
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                buffer.close();
+                reader.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return userList;
+    }
+
+    @Override
+    public void writeData(UserList userList) {
+
     }
 }
